f877b1af86673e9a2eeeaad3e64e32e6b6f7486b
Jonas Bourguignon <jonas.bourguignon@hotmail.com>
2024-11-08 11:00:02 +0100

    Gameboard and log

---
+++ 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,39 @@
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard({ onSelectSquare, turns }) {
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    /* const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedGameBoard
+        })
+
+        onSelectSquare();
+    } */
+
+    return <ol id="game-board">
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
+                {row.map((col, colIndex) => <li key={colIndex}>
+                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{col}</button>
+                </li>)}
+            </ol>
+        </li>)}
+    </ol>
+}
