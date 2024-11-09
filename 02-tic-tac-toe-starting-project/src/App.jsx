0e5d2fd17734802404ffd3ba8b72b440fbab904b
Jonas Bourguignon <xormios@hotmail.com>
2024-11-09 15:30:19 +0100

    GameOver and refactor

--- 02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,26 +1,92 @@
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
 import { useState } from "react"
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
+function deriveGameboard(gameTurns){
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players){
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = players[firstSquareSymbol];
+    }
+  }
+
+  return winner;
+}
 
 function App() {
-
-  const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState({
+    "X": "Player 1",
+    "O": "Player 2",
+  });
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameboard(gameTurns);
   
-  function handleSelectSquare(rowIndex, colIndex){
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === "X" ? "O" : "X");
-    
-    setGameTurns(prevTurns => {
+  const winner = deriveWinner(gameBoard, players);
 
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+  const hasDraw = gameTurns.length === 9 && !winner;
 
-      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer},...prevTurns];
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-      return updatedTurns
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
     });
   }
 
@@ -28,14 +94,25 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
