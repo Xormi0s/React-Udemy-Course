0153aa784d14199d10e54fa62093d6e37821b94b
Jonas Bourguignon <jonas.bourguignon@hotmail.com>
2025-01-17 15:35:48 +0100

    Part 1

--- 06-Project Management App/src/App.jsx
+++ 06-Project Management App/src/App.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
+
+  const [projectState, setProjectState] = useState({selectedProject: undefined, projects: []});
+
+  function handleStartAddProject(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
+
+  console.log(projectState);
+
+  let content;
+  
+  if (projectState.selectedProject === null) {
+    content = <NewProject  onAdd={handleAddProject}/>
+  } else if (projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
+
+  function handleAddProject(projectData){
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+        {content}
+      </main>
     </>
   );
 }
