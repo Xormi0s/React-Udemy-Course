727d63481b2a21f68ffae3f8868e77bff87b3e3e
Jonas Bourguignon <xormios@hotmail.com>
2025-02-26 20:10:07 +0100

    Part 1

--- 12-database connection/src/components/AvailablePlaces.jsx
+++ 12-database connection/src/components/AvailablePlaces.jsx
@@ -1,10 +1,47 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
+import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js'
+import { fetchAvaliablePlaces } from '../http.js'
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvaliablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+      } catch (error){
+        setError({message: error.message || "Could not fetch places, please try again later."});
+        setIsFetching(false);
+      }
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if(error) {
+    return <ErrorPage title="An error occured" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
---
+++ 12-database connection/src/http.js
@@ -0,0 +1,28 @@
+export async function fetchAvaliablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const restData = await response.json();
+
+    if (!response.ok){
+        throw new Error("Failed to fetch Places.");
+    }
+
+    return restData.places;
+}
+
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({places}),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    });
+    const restData = await response.json();
+
+    if (!response.ok){
+        throw new Error("Failed to update user data.");
+    }
+
+    return restData.message;
+}
